a79e6bbc4a84424de6d29cd133cbde92a5ae07d8
rbaron <rbaron@rbaron.net>
2021-12-24 19:12:57 +0100

    Day 24 cleanup

--- day24/main.py
+++ day24/main.py
@@ -1,66 +1,6 @@
 from functools import cache
 
 
-def run_instrs(input, instrs):
-    regs = {'x': 0, 'y': 0, 'z': 0, 'w': 0}
-
-    def eval_val(val):
-        if val in regs:
-            return regs[val]
-        return int(val)
-
-    for op, reg, val in instrs:
-        match op:
-            case 'inp':
-                regs[reg] = input.pop()
-                if regs['z'] % 26 != 0:
-                    return regs
-            case 'add':
-                regs[reg] += eval_val(val)
-            case 'mul':
-                regs[reg] *= eval_val(val)
-            case 'div':
-                regs[reg] //= eval_val(val)
-            case 'mod':
-                regs[reg] = regs[reg] % eval_val(val)
-            case 'eql':
-                regs[reg] = 1 if eval_val(val) == regs[reg] else 0
-    return regs
-
-
-def run_until_inp(instrs, pc, regs):
-    def eval_val(val):
-        if val in regs:
-            return regs[val]
-        return int(val)
-
-    while pc < len(instrs):
-        op, reg, val = instrs[pc]
-        match op:
-            case 'inp':
-                return regs, pc
-            case 'add':
-                regs[reg] += eval_val(val)
-            case 'mul':
-                regs[reg] *= eval_val(val)
-            case 'div':
-                regs[reg] //= eval_val(val)
-            case 'mod':
-                regs[reg] = regs[reg] % eval_val(val)
-            case 'eql':
-                regs[reg] = 1 if eval_val(val) == regs[reg] else 0
-        pc += 1
-
-    return regs, pc
-
-
-def run_level(digit, z, level, instrs):
-    pc = 1 + level * 18
-    regs = {'z': z, 'w': digit, 'x': 0, 'y': 0}
-    new_regs, new_pc = run_until_inp(instrs, pc, regs)
-    return new_regs['z']
-
-
 def run_level_fast(w, z, level, params):
     '''The instructions were extracted by hand from the raw instructions.
     See monad-annotated.txt for deets.'''
@@ -77,9 +17,6 @@ def find_model_numbers(params):
     @cache
     def find_solutions(level, z):
         if level == 14:
-            if z == 0:
-                print('ok!')
-                return [[]]
             return [[]] if z == 0 else []
 
         results = []
@@ -94,7 +31,6 @@ def find_model_numbers(params):
         return results
 
     all_sols = find_solutions(0, 0)
-    print(len(all_sols))
 
     # Part 1.
     print(''.join(map(str, max(all_sols))))
@@ -104,15 +40,12 @@ def find_model_numbers(params):
 
 
 if __name__ == '__main__':
-    def parse_line(line):
-        terms = line.strip().split(' ')
-        return terms + [None] * (3 - len(terms))
-
     with open('input.txt', 'r') as f:
-        instrs = [parse_line(l) for l in f]
+        instrs = [l.strip().split() for l in f]
 
     # Only three values vary among blocks of instructions between two `inp` instructions.
-    # These params are extract here for each block.
+    # These params are extract here for each block. They are in lines 4, 5 and 15 in each
+    # block.
     params = [
         (int(instrs[4 + i * 18][2]), int(instrs[5 + i * 18][2]),
          int(instrs[15 + i * 18][2]))
